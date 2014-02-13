312ff4073a4ec769b3d387220178156898183b98
Marcos Barreto <mbarreto@fing.edu.uy>
2014-02-12 23:57:25 -0200

    enum for job type.

---
+++ src/java/enums/EnumSatJobType.java
@@ -0,0 +1,21 @@
+package java.enums;
+
+/**
+ * Created by marbarfa on 2/12/14.
+ */
+public enum EnumSatJobType {
+
+    initial_configuration("init"),
+    iteration("rec");
+
+
+    private String type = null;
+
+    EnumSatJobType(String inputType){
+        this.type = inputType;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
