d9c03fbc1616da8a6f5415d9684e3c1d90b0d545
mbarreto <marbarfa@gmail.com>
2014-03-09 20:52:42 -0300

    implicit definitions Callback trait.

---
+++ src/main/scala/utils/ConvertionHelper.scala
@@ -0,0 +1,10 @@
+package scala.utils
+
+/**
+ * Created by marbarfa on 3/9/14.
+ */
+trait ConvertionHelper {
+
+  implicit def bool2int(b: Boolean) : Int = if (b) 1 else 0;
+  implicit def int2bool(b: Int) : Boolean = if (b==1) true else false;
+}
---
+++ src/main/scala/utils/ISatCallback.scala
@@ -0,0 +1,10 @@
+package scala.utils
+
+/**
+ * Created by marbarfa on 3/9/14.
+ */
+trait ISatCallback[T] {
+
+  def apply(t: T);
+
+}
